f1f08993430791144f99ff4bfa242244f499dc11
Elvis Serrão <elvis.serrao1@gmail.com>
2020-03-16 23:36:32 -0300

    Accept phone number for contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,6 +4,7 @@ class Contact < ApplicationRecord
   belongs_to :kind
 
   has_many :phones
+  accepts_nested_attributes_for :phones, reject_if: ->(attributes){ attributes['number'].blank? }, allow_destroy: true
 
   def kind_description
     return unless kind
@@ -11,18 +12,31 @@ class Contact < ApplicationRecord
     kind.description
   end
 
-  def to_br
-    {
-      name: name,
-      email: email,
-      birthdate: (I18n.l(birthdate) unless birthdate.blank?)
-    }
+  def phone_numbers
+    return unless phones
+
+    phones.map do |phone|
+      phone.number
+    end
   end
+
+  def birth_date
+    I18n.l(birthdate) unless birthdate.blank?
+  end
+
+  # def to_br
+  #   {
+  #     name: name,
+  #     email: email,
+  #     birthdate: (I18n.l(birthdate) unless birthdate.blank?)
+
+  #   }
+  # end
 
   def as_json(_options = {})
     super(
       only: %i[name email],
-      methods: %i[kind_description]
+      methods: %i[birth_date kind_description phone_numbers]
     )
   end
 end
