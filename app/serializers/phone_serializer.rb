72971e64f81adece3a9127a6e169e57678215efb
Elvis Serrão <elvis.serrao1@gmail.com>
2020-03-30 18:26:27 -0300

    Show related contact link for phones

--- app/serializers/phone_serializer.rb
+++ app/serializers/phone_serializer.rb
@@ -2,5 +2,7 @@
 
 class PhoneSerializer < ActiveModel::Serializer
   attributes :id, :number
-  belongs_to :contact
+  belongs_to :contact do
+    link(:related) { contact_url(object.contact.id) }
+  end
 end
