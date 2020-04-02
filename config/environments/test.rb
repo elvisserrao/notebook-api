5a65217534707cf206d3a7f9c017454ce1ef8fac
Elvis Serrão <elvis.serrao1@gmail.com>
2020-04-02 00:14:31 -0300

    Set default url options

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,6 +6,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  Rails.application.routes.default_url_options = {
+    host: 'localhost',
+    port: 3000
+  }
   config.cache_classes = false
 
   # Do not eager load code on boot. This avoids loading your whole application
